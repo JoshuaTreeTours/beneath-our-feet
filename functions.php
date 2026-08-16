766dded0f141e917bd34aef24e758a7039658891
JoshuaTreeTours <adventurehummer@gmail.com>
2026-08-16 10:02:27 -0700

    Support editable Gutenberg homepage content

--- functions.php
+++ functions.php
@@ -11,6 +11,8 @@ function bof_setup() {
     add_theme_support( 'title-tag' );
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'responsive-embeds' );
+    add_theme_support( 'align-wide' );
+    add_theme_support( 'editor-styles' );
     add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption', 'style', 'script' ) );
 
     register_nav_menus(
