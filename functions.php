a65b60002d27a65d49d7ade80f6ca63097b701e8
JoshuaTreeTours <adventurehummer@gmail.com>
2026-08-16 17:33:18 -0700

    Activate topic menu routing

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@ require_once get_stylesheet_directory() . '/inc/national-park-library.php';
 require_once get_stylesheet_directory() . '/inc/all-media-import.php';
 require_once get_stylesheet_directory() . '/inc/national-park-reconcile.php';
 require_once get_stylesheet_directory() . '/inc/topic-library.php';
+require_once get_stylesheet_directory() . '/inc/menu-topic-redirects.php';
 
 function bof_setup() {
     add_theme_support( 'title-tag' );
