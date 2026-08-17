bd280b89d32eff1554c6e9b0716c477ff52a9cfd
JoshuaTreeTours <adventurehummer@gmail.com>
2026-08-17 08:43:30 -0700

    Finalize footer tagline and social icons

--- footer.php
+++ footer.php
@@ -1,9 +1,13 @@
 </main>
 <footer class="site-footer">
 <div class="bof-footer-inner">
-<a class="bof-footer-social" href="https://www.facebook.com/BeneathOurFeetEarth/" target="_blank" rel="noreferrer noopener" aria-label="Facebook"><span aria-hidden="true">f</span></a>
+<a class="bof-footer-social" href="https://www.facebook.com/BeneathOurFeetEarth/" target="_blank" rel="noreferrer noopener" aria-label="Beneath Our Feet on Facebook">
+<svg viewBox="0 0 24 24" width="22" height="22" aria-hidden="true" focusable="false"><path fill="currentColor" d="M13.5 22v-9h3l.45-3.5H13.5V7.26c0-1.01.28-1.7 1.73-1.7H17V2.43c-.31-.04-1.37-.13-2.61-.13-2.58 0-4.35 1.58-4.35 4.48V9.5H7.12V13h2.92v9h3.46Z"/></svg>
+</a>
 <p class="bof-footer-tagline">One planet. Billions of years. Countless stories</p>
-<a class="bof-footer-social bof-instagram" href="https://www.instagram.com/beneathourfeet.earth/" target="_blank" rel="noreferrer noopener" aria-label="Instagram"><span aria-hidden="true">◎</span></a>
+<a class="bof-footer-social bof-instagram" href="https://www.instagram.com/beneathourfeet.earth/" target="_blank" rel="noreferrer noopener" aria-label="Beneath Our Feet on Instagram">
+<svg viewBox="0 0 24 24" width="22" height="22" aria-hidden="true" focusable="false"><path fill="currentColor" d="M7 2h10a5 5 0 0 1 5 5v10a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5Zm0 2a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3H7Zm10.5 1.5a1.25 1.25 0 1 1 0 2.5 1.25 1.25 0 0 1 0-2.5ZM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 2a3 3 0 1 0 0 6 3 3 0 0 0 0-6Z"/></svg>
+</a>
 </div>
 </footer>
 <?php wp_footer(); ?>
