bee8e1705d9af55ac91a786174d2df5842e00e3d
JoshuaTreeTours <adventurehummer@gmail.com>
2026-08-17 06:32:38 -0700

    Apply Source Material title cleanup

--- functions.php
+++ functions.php
@@ -148,7 +148,7 @@ function bof_seed_source_material_page() {
         return;
     }
 
-    $version  = 3;
+    $version  = 4;
     $existing = get_page_by_path( 'source-material', OBJECT, 'page' );
 
     if ( ! $existing ) {
